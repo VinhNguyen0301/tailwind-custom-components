754090b06d484256a4b2415c13578b988fb04cc8
VinhNguyen0301 <nguyenvinh.fit@gmail.com>
2023-11-15 14:08:47 +0700

    Add some tailwind config prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="font-bold text-3xl underline">
       Hello world!
     </h1>
   )
 }
 
 export default App
+   
