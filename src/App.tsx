cd9980ef96d57d75f5c36b17092e9642781c787f
VinhNguyen0301 <nguyenvinh.fit@gmail.com>
2023-11-15 15:33:30 +0700

    Add UserProfile Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
+import UserProfile from "./Component-1/UserProfile";
 
 function App() {
-
   return (
-    <h1 className="font-bold text-3xl underline">
-      Hello world!
+    <h1 className="flex h-screen w-full items-center justify-center bg-[#E6EFFA]">
+      <UserProfile />
     </h1>
-  )
+  );
 }
 
-export default App
-   
+export default App;
