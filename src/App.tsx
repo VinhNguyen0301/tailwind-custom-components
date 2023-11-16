a7221879b78b3e822f708d332171767068f3a2b1
VinhNguyen0301 <nguyenvinh.fit@gmail.com>
2023-11-16 13:54:12 +0700

    Add component List collection responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import UserProfile from "./Component-1/UserProfile";
 import JobList from "./Component-2/JobList";
+import Collections from "./Component-3/Collections";
 
 function App() {
   return (
-    <h1 className="flex h-screen w-full items-center justify-center bg-[#E6EFFA]">
+    <h1 className="flex  w-full items-center justify-center bg-[#E6EFFA]">
       {/* <UserProfile /> */}
-      <JobList />
+      {/* <JobList /> */}
+      <Collections />
     </h1>
   );
 }
